02f58dd052108a3c8ea41fc1ca5668c6fba33b8e
Takeru Yoshihara <takeru@notainc.com>
2019-01-10 11:20:12 +0900

    svg以外のファイルをレンダリング対象から除外

--- example/icon-html.js
+++ example/icon-html.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const svgDir = './svg'
 
-// svgファイルが存在するアイコンを表示するためのhtmlを生成
-// fsモジュールがブラウザでは使えないため、Node.jsで実行している
+// svgファイルが存在するアイコンを表示するためのhtmlを生成するスクリプト
+// fsモジュールがブラウザでは使えないため、Node.jsで実行
 const svgFileNames = fs.readdirSync(svgDir)
 let html = ''
 svgFileNames.forEach(function (fileName) {
+  if (!fileName.match(/svg/)) return
   const id = fileName.replace('.svg', '')
   html += `<li><svg class="icon"><use xlink:href="#${id}"></use></svg></li>`
 })
